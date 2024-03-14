a6ec0b063328cc9db1a1fe163c955e716d97bc20
Jacqueline <jacquigavi921@gmail.com>
2024-03-14 01:28:16 -0500

    Inicializacion de controller

--- src/main/java/com/banquito/core/pagos/controller/PagoController.java
+++ src/main/java/com/banquito/core/pagos/controller/PagoController.java
@@ -1,6 +1,24 @@
 package com.banquito.core.pagos.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.banquito.core.pagos.service.CuentaService;
+import com.banquito.core.pagos.service.TransaccionService;
+
+@RestController
+@RequestMapping("/api/v1/cuentas")
 public class PagoController {
-    
+
+    private final CuentaService cuentaService;
+    private final TransaccionService transaccionService;
+
+
+    public PagoController(CuentaService cuentaService, TransaccionService transaccionService) {
+        this.cuentaService = cuentaService;
+        this.transaccionService = transaccionService;
+    }
 }
-AS
